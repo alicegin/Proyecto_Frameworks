f2f26fd47aa715e870b79b842a820f5e8e98b460
alicegin <ordazr.ximena@gmail.com>
2023-10-18 09:41:53 -0600

    Agregar funcionalidad a crear_cuenta e inicio_sesion

--- inicio/urls.py
+++ inicio/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 urlpatterns=[
-path('',views.inicio_sesion, name='inicio_sesion'),
-path('',views.home, name='home')
+path('',views.crear_cuenta, name='crear_cuenta'),
+path('iniciosesion/',views.inicio_sesion, name='inicio_sesion'),
+path('home/',views.home, name='home'),
+path('cerrarsesion/',views.cerrar_sesion, name='cerrar_sesion')
 ]
--- inicio/views.py
+++ inicio/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.models import User
 from django.contrib.auth import login,logout, authenticate
+from django.http import HttpResponse
+from django.db import IntegrityError
 # Create your views here.
 def inicio_sesion(request):
     if request.method == 'GET':
@@ -8,15 +11,38 @@ def inicio_sesion(request):
             'form': AuthenticationForm
         })
     else:
-        usuario= authenticate(request, usuario=request.POST['Usuario'], password=request.POST['Contrasena'])
+        usuario= authenticate(request, username=request.POST['Usuario'], password=request.POST['Contrasena'])
         if usuario is None:
             return render(request,'inicio_sesion.html', {
                 'form': AuthenticationForm,
-                'Error': '<div class="alert alert-danger" role="alert">Contraseña o usuario inválido </div>'
+                'error': '<div class="alert alert-danger" role="alert">Contraseña o usuario inválido </div>'
             })
         else:
             login(request,usuario)
             return redirect('home')
+
+def cerrar_sesion(request):
+    logout(request)
+    return redirect("inicio_sesion")
     
 def home(request):
     return render(request,'home.html')
+
+def crear_cuenta(request):
+    if request.method=='GET':
+        return render(request, 'crear_cuenta.html', {
+            'form': UserCreationForm
+        })
+    else:
+        try:
+            usuario=User.objects.create_user(username=request.POST['Usuario'],password=request.POST['Contrasena'], 
+            email=request.POST['Correo'])
+            usuario.save()
+            login(request,usuario)
+            return redirect('home')
+        except IntegrityError:
+            return render(request, 'crear_cuenta.html', {
+            'form': UserCreationForm,
+            'error': '<div class="alert alert-danger" role="alert">El usuario ya existe. Intente con uno nuevo. </div>'
+        })
+       
